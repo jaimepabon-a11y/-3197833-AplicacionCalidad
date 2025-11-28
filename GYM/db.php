6ea36aedb7618ccf0683c4cae9e2f2cbe66f9744
jaimepabon-a11y <jaimepabon4k@gmail.com>
2025-11-28 15:46:46 -0500

    Update db.php

--- GYM/db.php
+++ GYM/db.php
@@ -1,6 +1,7 @@
 <?php
 // GYM/GYM/db.php
-$pdo = new PDO('mysql:host=localhost;dbname=gym_db', 'root', '1234');
+$db_password = getenv('DB_PASSWORD'); 
+$pdo = new PDO('mysql:host=localhost;dbname=gym_db', 'root', $db_password);
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $pdo->exec("set names utf8");
 ?>
