21812706ff9948e7dd6afe5aeb1469876f80af80
jaimepabon-a11y <jaimepabon4k@gmail.com>
2025-11-28 15:43:32 -0500

    Change database password in db.php
    Updated database password from an empty string to '1234'.

--- GYM/db.php
+++ GYM/db.php
@@ -1,6 +1,6 @@
 <?php
 // GYM/GYM/db.php
-$pdo = new PDO('mysql:host=localhost;dbname=gym_db', 'root', '');
+$pdo = new PDO('mysql:host=localhost;dbname=gym_db', 'root', '1234');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $pdo->exec("set names utf8");
 ?>
